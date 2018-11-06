81ffe08382126367459f3e833eb26712562c0d15
Chanon Jenakom <chanonjenakom@gmail.com>
2018-11-06 14:57:16 +0700

    Add 'draw()' method for Maze

--- src/maze/maze.js
+++ src/maze/maze.js
@@ -11,14 +11,18 @@ class Maze {
     for (let row = 0; row < this.numRows; row++) {
       this.cells[row] = [];
       for (let col = 0; col < this.numCols; col++) {
-        if (col === this.source.col && row === this.source.row) {
+        if (col === this.source.COL && row === this.source.ROW) {
           this.cells[row][col] = new Cell(row, col, COLORS.SOURCE);
-        } else if (col === this.destination.col && row === this.destination.row) {
+        } else if (col === this.destination.COL && row === this.destination.ROW) {
           this.cells[row][col] = new Cell(row, col, COLORS.DESTINATION);
         } else {
           this.cells[row][col] = new Cell(row, col);
         }
       }
     }
+  }
+
+  draw(sketch) {
+    this.cells.reduce((a, b) => a.concat(b), []).forEach((cell) => cell.draw(sketch));
   }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,10 +1,14 @@
 var mySketch = new p5((sketch) => {
 
+  let maze;
+
   sketch.setup = () => {
-    sketch.createCanvas(CANVAS_DIMEN.WIDTH, CANVAS_DIMEN.HEIGHT);
+    sketch.createCanvas(CANVAS_DIMEN.WIDTH + 1, CANVAS_DIMEN.HEIGHT + 1);
+
+    maze = new Maze(MAZE_DIMEN.NUM_ROWS, MAZE_DIMEN.NUM_COLS, SOURCE, DESTINATION);
   };
 
   sketch.draw = () => {
-    sketch.background(COLORS.NO_PATH);
+    maze.draw(sketch);
   };
 }, SKETCH_NAME);
