8d1519a67c470e9d8d4f26b74e9c530ce30549e7
Chanon Jenakom <chanonjenakom@gmail.com>
2018-11-06 14:23:26 +0700

    Create 'draw()' method

--- src/maze/cell.js
+++ src/maze/cell.js
@@ -6,13 +6,33 @@ class Cell {
     this.x = this.col * CELL_DIMEN.WIDTH + OFFSET.X;
     this.x = this.row * CELL_DIMEN.HEIGHT + OFFSET.Y;
 
-    this.color = COLORS.WALL;
-    
+    this.color = COLORS.NO_PATH;
+
     this.neighbors = {
       top: null,
       right: null,
       bottom: null,
       left: null
     };
+  }
+
+  draw(sketch) {
+    sketch.noStroke();
+    sketch.fill(this.color);
+    sketch.rect(this.x, this.y, CELL_DIMEN.WIDTH, CELL_DIMEN.HEIGHT);
+
+    sketch.stroke(COLORS.WALL);
+    if (!this.neighbors.top) {
+      line(this.x, this.y, this.x + CELL_DIMEN.WIDTH, this.y);
+    }
+    if (!this.neighbors.right) {
+      line(this.x + CELL_DIMEN.WIDTH, this.y, this.x + CELL_DIMEN.WIDTH, this.y + CELL_DIMEN.HEIGHT);
+    }
+    if (!this.neighbors.bottom) {
+      line(this.x + CELL_DIMEN.WIDTH, this.y + CELL_DIMEN.HEIGHT, this.x, this.y + CELL_DIMEN.HEIGHT);
+    }
+    if (!this.neighbors.left) {
+      line(this.x, this.y + CELL_DIMEN.HEIGHT, this.x, this.y);
+    }
   }
 }
