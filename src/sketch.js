8d937ff2cedcf541234d1adbdf2c7bdb7b22583c
Chanon Jenakom <chanonjenakom@gmail.com>
2018-11-10 08:44:59 +0700

    Limit framerate to 5 FPS

--- src/sketch.js
+++ src/sketch.js
@@ -4,6 +4,7 @@ var mySketch = new p5((sketch) => {
 
   sketch.setup = () => {
     sketch.createCanvas(CANVAS_DIMEN.WIDTH + 1, CANVAS_DIMEN.HEIGHT + 1);
+    sketch.frameRate(FRAMERATE);
 
     maze = new Maze(MAZE_DIMEN.NUM_ROWS, MAZE_DIMEN.NUM_COLS, SOURCE.ROW, SOURCE.COL, DESTINATION.ROW, DESTINATION.COL);
   };
